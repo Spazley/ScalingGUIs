db3127eeb26f48717a90bc9afde97583ef4616dc
Spazley <36523721+Spazley@users.noreply.github.com>
2019-04-10 00:05:34 -0500

    Update

--- src/main/java/spazley/scalingguis/ScalingGUIs.java
+++ src/main/java/spazley/scalingguis/ScalingGUIs.java
@@ -17,7 +17,7 @@ public class ScalingGUIs
 {
     public static final String MODID = "scalingguis";
     public static final String NAME = "ScalingGUIs";
-    public static final String VERSION = "1.12.2-0.0.3.0";
+    public static final String VERSION = "1.12.2-0.0.3.1";
 
     public static Logger logger;
 
